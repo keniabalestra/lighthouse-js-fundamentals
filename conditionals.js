f381c710753831a6bb468d0464218ad1eb66eab8
Kenia Balestra <kenia.balestra@gmail.com>
2022-07-20 19:18:58 +0000

    Add more conditionals exemples

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@
 
 // console.log("Now you're ready to go ourside!");
 
-
+//***************************** */
 // const cold = false;
 
 // if (cold) {
@@ -20,14 +20,21 @@
 //   console.log("Short sleeves are fine.");
 // }
 
-const temperature = 20;
+//****************
+// const temperature = 20;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+//**********************************
+if (!raining){
+  console.log("Leave your umbrella at home!");
 }
-
-console.log("Now you're ready to go outside!");
