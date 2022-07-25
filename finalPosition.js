c4b34e825e6e3ded149c38fb262f1c4e2bfde182
Kenia Balestra <kenia.balestra@gmail.com>
2022-07-25 16:39:10 +0000

    Voting station and coordenadas

---
+++ finalPosition.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+
+
+function finalPosition(moves) {
+  let position = [0, 0];
+  for (const move of moves) {
+    if (move === 'north') {
+      position[1] = position[1] + 1
+    }
+    if (move === 'south') {
+      position[1] = position[1] - 1
+    }
+    if (move === 'west') {
+      position[0] = position[0] - 1
+    }
+    if (move === 'east') {
+      position[0] = position[0] + 1
+    }
+    
+  }
+  return position;
+}
+console.log(finalPosition(moves));
